52324fd226d10e383fe1751bcf6d851c9e339659
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2023-11-28 17:31:30 +0000

    Added basic role base authentication

--- Auth/src/main/java/com/muskan/Auth/SecurityConfiguration.java
+++ Auth/src/main/java/com/muskan/Auth/SecurityConfiguration.java
@@ -1,17 +1,38 @@
 package com.muskan.Auth;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //this web security configurer, other is method level
+//create a bean of type password Encoder
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     //WebSecurityConfigurerAdapte
-    @Override
+    // @Override
+    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //     auth.inMemoryAuthentication()
+    //         .withUser("blah")
+    //         .password(passwordEncoder().encode("blah"))
+    //         .roles("USER");
+    // }
+
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
+     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
                         .withUser("blah")
                         .password("blah")
                         .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance(); //it does nothing
     }
 }

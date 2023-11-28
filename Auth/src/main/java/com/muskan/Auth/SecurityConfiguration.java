471facd2bda5c708d48db9b6572bd5158df5de42
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2023-11-28 18:17:25 +0000

    Added Authorization using HttpSecurity

--- Auth/src/main/java/com/muskan/Auth/SecurityConfiguration.java
+++ Auth/src/main/java/com/muskan/Auth/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.muskan.Auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -23,16 +24,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     // public PasswordEncoder passwordEncoder() {
     //     return new BCryptPasswordEncoder();
     // }
-     @Override
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
                         .withUser("blah")
                         .password("blah")
-                        .roles("USER");
+                        .roles("USER")
+                        .and()
+                        .withUser("foo")
+                        .password("foo")
+                        .roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance(); //it does nothing
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                        .antMatchers("/**").hasRole("USER")
+                        .and().formLogin();
     }
 }

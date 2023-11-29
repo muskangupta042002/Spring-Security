f2aa5ba6f1aac3096eab901229f30e55b38e8e4b
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2023-11-29 10:35:25 +0000

    JDBC authentication

--- Auth/src/main/java/com/muskan/Auth/SecurityConfiguration.java
+++ Auth/src/main/java/com/muskan/Auth/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.muskan.Auth;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 //create a bean of type password Encoder
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    DataSource dataSource;
     //WebSecurityConfigurerAdapte
     // @Override
     // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -26,14 +33,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     // }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                        .withUser("blah")
-                        .password("blah")
-                        .roles("USER")
-                        .and()
-                        .withUser("foo")
-                        .password("foo")
-                        .roles("ADMIN");
+        // auth.inMemoryAuthentication()
+        //                 .withUser("blah")
+        //                 .password("blah")
+        //                 .roles("USER")
+        //                 .and()
+        //                 .withUser("foo")
+        //                 .password("foo")
+        //                 .roles("ADMIN");
+
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .withDefaultSchema()
+            .withUser(
+                User.withUsername("user")
+                    .password("pass")
+                    .roles("USER")
+            )
+            .withUser(
+                User.withUsername("admin")
+                    .password("pass")
+                    .roles("ADMIN")
+            )
+        ;
     }
 
     @Bean
